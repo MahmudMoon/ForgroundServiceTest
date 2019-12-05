b23a7d0cb07ef55138a45bb72a0524633fcba6fb
Mustofa Mahmud <mooncseru14@gmail.com>
2019-12-06 00:03:02 +0600

    forground service, implemented media control buttons

--- app/src/main/java/com/example/forgroundservicetest/NotificationService.java
+++ app/src/main/java/com/example/forgroundservicetest/NotificationService.java
@@ -9,10 +9,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.MediaCas;
 import android.media.MediaPlayer;
-import android.media.session.MediaSession;
 import android.os.Build;
 import android.os.IBinder;
 import android.provider.Settings;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import androidx.media.MediaSessionManager;
 
 import static com.example.forgroundservicetest.app.CHANNEL_ID;
 import static com.example.forgroundservicetest.app.NOTIFICATION_ID;
@@ -29,7 +31,6 @@ import static com.example.forgroundservicetest.app.NOTIFICATION_ID;
 public class NotificationService extends Service {
     private  NotificationManager notificationManager;
     MediaPlayer mediaPlayer;
-    MediaSession mSession;
     private static final String TAG = "NotificationService";
     @Override
     public void onCreate() {
@@ -38,19 +39,6 @@ public class NotificationService extends Service {
         createNotificationChannel();
         mediaPlayer = MediaPlayer.create(getApplicationContext(), Settings.System.DEFAULT_RINGTONE_URI);
         mediaPlayer.setLooping(true);
-        mSession = new MediaSession(this, "MusicService");
-        mSession.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS | MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
-
-        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.i(TAG, "onReceive: ");
-                Toast.makeText(getApplicationContext(),"Play",Toast.LENGTH_SHORT).show();
-            }
-        };
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("play");
-        registerReceiver(broadcastReceiver,intentFilter);
     }
 
     @Override
@@ -69,29 +57,21 @@ public class NotificationService extends Service {
         //intent for play noti
         Intent intent1 = new Intent(this,MainActivity.class);
         intent1.putExtra("data","play");
-        PendingIntent pendingIntent1 = PendingIntent.getActivity(this,107,intent1,PendingIntent.FLAG_CANCEL_CURRENT);
-
-        //intent for pause noti
-        Intent intent2 = new Intent(this,MainActivity.class);
-        intent1.putExtra("data","pause");
-        PendingIntent pendingIntent2 = PendingIntent.getActivity(this,108,intent2,PendingIntent.FLAG_NO_CREATE);
-
-        //intent for stop noti
-        Intent intent3 = new Intent(this,MainActivity.class);
-        intent1.putExtra("data","pause");
-        PendingIntent pendingIntent3 = PendingIntent.getActivity(this,109,intent3,PendingIntent.FLAG_ONE_SHOT);
-
-
+        intent1.setAction("play");
+        PendingIntent pendingIntent1 = PendingIntent.getBroadcast(getApplicationContext(),107,intent1,PendingIntent.FLAG_UPDATE_CURRENT);
+        //PendingIntent pendingIntent1 = PendingIntent.getActivity(getApplicationContext(),107,intent1,PendingIntent.FLAG_NO_CREATE);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.capture);
         NotificationCompat.Builder builder = new
                 NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
                 .setContentTitle("Demo Title")
                 .setContentText("Here is the body")
-                .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
-                .addAction(R.drawable.ic_android,"Play",pendingIntent1)
-                .addAction(R.drawable.ic_android,"Pause",pendingIntent2)
-                .addAction(R.drawable.ic_android,"Stop",pendingIntent3)
-                .setSmallIcon(R.drawable.ic_android);
+                .setSmallIcon(R.drawable.ic_android)
+                .setLargeIcon(bitmap)
+                .addAction(R.drawable.ic_skip_previous_black_24dp,"previous",null)
+                .addAction(R.drawable.ic_play_arrow_black_24dp,"play",null)
+                .addAction(R.drawable.ic_skip_next_black_24dp,"next",null)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle());
         return builder;
     }
 
